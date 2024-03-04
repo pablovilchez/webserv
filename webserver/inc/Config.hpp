270dec8c7bca1b3d170d049fcb71026b36c81a0c
pablovilchez <pablovilchez.r@gmail.com>
2024-03-04 21:55:56 +0100

    Continue parsing

--- webserver/inc/Config.hpp
+++ webserver/inc/Config.hpp
@@ -15,19 +15,20 @@ class Config
 		void printData();
 
 		typedef std::map<int, std::string> ErrorPageMap;
+		typedef std::map<std::string, std::set<std::string> > RoutesMap;
 		std::string getErrorPage(int errorCode);
 
 	private:
-		int _port;
-		std::string _serverName;
-		ErrorPageMap _errorPages;
-		std::string _hostAddress;
-		std::string _errorPages;
-		int _maxBodySize;
+		int				_port;
+		std::string		_serverName;
+		ErrorPageMap	_errorPages;
+		std::string		_hostAddress;
+		std::string		_errorPages;
+		int				_maxBodySize;
 
-		std::string _defaultFolder;
-		std::string _defaultFile;
-		std::map<std::string, std::set<std::string> > routes;
+		std::string		_defaultFolder;
+		std::string		_defaultFile;
+		RoutesMap		_routes;
 
 };
 
