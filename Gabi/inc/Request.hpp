6e0133c874fb54a1a552831338d85503bbeebfcf
Gabriela <krustagabriela@gmail.com>
2024-03-05 17:16:50 +0100

    on upload file

--- Gabi/inc/Request.hpp
+++ Gabi/inc/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 21:12:49 by pvilchez          #+#    #+#             */
-/*   Updated: 2024/03/04 17:10:44 by gkrusta          ###   ########.fr       */
+/*   Updated: 2024/03/05 16:48:39 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define REQUEST_HPP
 
 # include <iostream>
+#include <fstream>
+#include <unistd.h>
 
 class Request
 {
@@ -21,8 +23,9 @@ class Request
 		Request(const std::string &raw);
 		~Request();
 
-		void parseContent();
-		void printData();
+		void	parseContent();
+		void	parseBody();
+		void	printData();
 		
 		std::string getRaw() const;
 /* 		std::string getHeader() const;
@@ -31,20 +34,15 @@ class Request
 		std::string getExtension() const; */
 
 	private:
-/* 		std::string _raw;
-		std::string _header;
-		std::string _body;
-		std::string _path;
-		std::string _extension; */
 		std::string _raw;
 		std::string _method;
 		std::string _path;
 		std::string _version;
 		std::string _host;
 		std::string _contentType;
-		int			 _contentLength;
+		size_t			 _contentLength;
 		std::string _body;
-		// int		_number;
+		int			_number;
 };
 
 #endif
--- Gabi/src/Request.cpp
+++ Gabi/src/Request.cpp
@@ -6,15 +6,22 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 21:18:22 by pvilchez          #+#    #+#             */
-/*   Updated: 2024/03/04 17:34:43 by gkrusta          ###   ########.fr       */
+/*   Updated: 2024/03/05 17:16:00 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Request.hpp"
-#include <iostream>
 
 Request::Request(const std::string &raw) : _raw(raw)
 {
+	_method = "";
+	_path = "";
+	_version = "";
+	_host = "";
+	_contentType = "";
+	_contentLength = 0;
+	_body = "";
+	_number = 0;
 	parseContent();
 }
 
@@ -24,12 +31,54 @@ std::string Request::getRaw() const
 {
 	return _raw;
 }
+#include <vector>
+
+void Request::parseBody() {
+    // Check if Content-Length header is present
+    if (_contentLength == 0) {
+        std::cerr << "Content-Length header is missing or zero." << std::endl;
+        return;
+    }
+
+    // Find the start of the body
+    std::string boundary = "\r\n\r\n";
+	size_t bodyStart = _raw.find(boundary) + boundary.size();
+
+    // Create a vector with the size of the content length
+    std::vector<char> bodyData(_contentLength);
+
+    // Copy the binary data from the body into the vector
+    std::copy_n(_raw.begin() + bodyStart, _contentLength, bodyData.begin());
+
+    // Identify content type
+    std::string fileExtension;
+    if (_contentType.find("image/jpeg") != std::string::npos) {
+        fileExtension = ".jpg";
+    } else if (_contentType.find("image/png") != std::string::npos) {
+        fileExtension = ".png";
+    } else {
+        std::cerr << "Unsupported content type." << std::endl;
+        return;
+    }
+
+    // Save the file with the appropriate extension
+    std::ofstream outputFile("uploaded_image" + fileExtension, std::ios::binary);
+    if (outputFile.is_open()) {
+        // Write the binary data to the file
+        outputFile.write(bodyData.data(), bodyData.size());
+        outputFile.close();
+        std::cout << "File saved: uploaded_image" << fileExtension << std::endl;
+    } else {
+        std::cerr << "Error opening file for writing." << std::endl;
+    }
+}
 
 void Request::parseContent()
 {
 	size_t	pos = 0;
 	size_t	end = _raw.find("\r\n");
-
+	std::cout << "len :\n" << _raw << std::endl;
+	std::cout << "Raw Request:\n" << _contentLength << std::endl;
 	// parse request line
 	if (end != std::string::npos) {
 		std::string	line = _raw.substr(pos, end - pos);
@@ -41,15 +90,12 @@ void Request::parseContent()
 			_path = line.substr(space1 + 1, space2 - space1 - 1);
 			_version = line.substr(space2 + 1);
 		}
-			pos = end + 2; // Move to the next position after "\r\n"
+		pos = end + 2; // Move to the next position after "\r\n"
 	}
 
 	// parse header
-	while ((end = _raw.find("\r\n")) != std::string::npos && pos < _raw.size()) {
+	while ((end = _raw.find("\r\n", pos)) != std::string::npos && pos < _raw.size()) {
 		std::string	line = _raw.substr(pos, end - pos);
-		    std::cout << "Line: " << line << std::endl;
-    		std::cout << "Pos: " << pos << ", End: " << end << ", Size: " << _raw.size() << std::endl;
-		pos = end + 2;
 		size_t	colPos = line.find(':');
 		if (colPos != std::string::npos) {
 			std::string	key = line.substr(0, colPos);
@@ -61,15 +107,10 @@ void Request::parseContent()
 			else if (key == "Content-Length")
 				_contentLength = atoi(value.c_str());
 		}
-		else
-			break ;
+		pos = end + 2;
 	}
-/* 	size_t dotPos = _path.rfind('.');
-	if (dotPos != std::string::npos) 
-		_contentType = _path.substr(dotPos + 1); */
-	if (_contentLength > 0) {
-		_body = _raw.substr(pos, _contentLength);
-	}
+	if (_method == "POST")
+		parseBody();
 }
 
 void Request::printData()
@@ -80,9 +121,7 @@ void Request::printData()
 	std::cout << "Request _host:    " << _host << std::endl;
 	std::cout << "Request _contentType:    " << _contentType << std::endl;
 	std::cout << "Request _contentLength:    " << _contentLength << std::endl;
-	std::cout << "Request _body:    " << _body << std::endl;
 
-	std::cout << std::endl;
 }
 
 /* std::string Request::getHeader() const
